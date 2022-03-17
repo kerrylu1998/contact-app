c626b847f5a891deac5ab1f1159bada9aed2ddc8
KerryLu <lud34567@myumanitoba.ca>
2022-03-17 00:49:15 -0500

    add and delete function

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -23,21 +23,25 @@ class AddContact extends React.Component {
                 <form className="ui form" onSubmit={this.add}>
                     <div className="field">
                         <label>Name</label>
-                        <input type="text" name="name" placeholder="Name" 
-                        value={this.state.name}
-                        onChange={ (e) => this.setState({name: e.target.value})}></input>
+                        <input 
+                            type="text" name="name" placeholder="Name" 
+                            value={this.state.name}
+                            onChange={ (e) => this.setState({ name: e.target.value })}
+                        />
                     </div>
                     <div className="field">
                         <label>Email</label>
-                        <input type="text" name="email" placeholder="Email"
-                        value={this.state.email}
-                        onChange={ (e) => this.setState({email: e.target.value})}></input>
+                        <input 
+                            type="text" name="email" placeholder="Email"
+                            value={this.state.email}
+                            onChange={(e) => this.setState({ email: e.target.value })}
+                        />
                     </div>
-                    <button className="ui button blue"> Add</button>
+                    <button className="ui button blue">Add</button>
                 </form>
             </div>
         );
     }
 }
 
-export default AddContact
+export default AddContact;
