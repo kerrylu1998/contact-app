a8e8ab97375058de6f6a9f980096f7a62c745d48
KerryLu <lud34567@myumanitoba.ca>
2022-03-16 23:01:45 -0500

    Added contact cards and contact lists

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import "./App.css";
 import Header from "./Header";
 import AddContact from "./AddContact";
 import ContactList from "./ContactList";
 
 function App() {
-  const contacts =[
-    {
-       id: "1",
-       name: "Dipesh",
-       email: "malvia@gmail.com",
-    },
-    {
-      id: "2",
-      name: "Nikesh",
-      email: "nicks@gmail.com",
-   },
-  ]
+  const LOCAL_STOREAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    setContacts([...contacts, contact])
+  }
+
+  useEffect(() => {
+    const retrieveContacts = JSON.parse(localStorage.getItem(LOCAL_STOREAGE_KEY));
+    if(retrieveContacts)
+      setContacts(retrieveContacts);
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STOREAGE_KEY, JSON.stringify(contacts))
+  }, [contacts])
 
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={ contacts }/>
     </div>
   );
